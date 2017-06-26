ca1590ec0be1b35e0237769032d4141ebfecf832
骆大峰 <luodafeng@hqyxjy.com>
2017-06-26 16:45:47 +0800

    开始重构

---
+++ calendar/src/main/java/com/ldf/calendar/view/CalendarAttr.java
@@ -0,0 +1,69 @@
+package com.ldf.calendar.view;
+
+/**
+ * Created by ldf on 17/6/26.
+ */
+
+public class CalendarAttr {
+    /**
+     * 以何种方式排列星期:<br/>
+     * {@link com.ldf.calendar.view.CalendarAttr.WeekArrayType} <br/>
+     */
+    private WeekArrayType weekArrayType;
+
+    /**
+     * 日历才去周布局或者月布局:<br/>
+     * {@link com.ldf.calendar.view.CalendarAttr.CalendayType} 布局类型<br/>
+     */
+    private CalendayType calendarType;
+
+    /**
+     * 日期格子高度
+     */
+    private int cellHeight;
+
+    /**
+     * 日期格子宽度
+     */
+    private int cellWidth;
+
+    public WeekArrayType getWeekArrayType() {
+        return weekArrayType;
+    }
+
+    public void setWeekArrayType(WeekArrayType weekArrayType) {
+        this.weekArrayType = weekArrayType;
+    }
+
+    public CalendayType getCalendarType() {
+        return calendarType;
+    }
+
+    public void setCalendarType(CalendayType calendarType) {
+        this.calendarType = calendarType;
+    }
+
+    public int getCellHeight() {
+        return cellHeight;
+    }
+
+    public void setCellHeight(int cellHeight) {
+        this.cellHeight = cellHeight;
+    }
+
+    public int getCellWidth() {
+        return cellWidth;
+    }
+
+    public void setCellWidth(int cellWidth) {
+        this.cellWidth = cellWidth;
+    }
+
+    public enum WeekArrayType {
+        Sunday , Monday
+    }
+
+    public enum CalendayType {
+        WEEK , MONTH
+    }
+}
