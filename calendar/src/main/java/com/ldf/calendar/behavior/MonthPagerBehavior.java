d5354fac2442e9a7be17737c08fe00bc24bc7a06
keaideluren <513421345@qq.com>
2018-02-02 14:03:14 +0800

    增加在日历控件上滑下滑事件

--- calendar/src/main/java/com/ldf/calendar/behavior/MonthPagerBehavior.java
+++ calendar/src/main/java/com/ldf/calendar/behavior/MonthPagerBehavior.java
@@ -3,6 +3,7 @@ package com.ldf.calendar.behavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.ldf.calendar.Utils;
@@ -28,6 +29,129 @@ public class MonthPagerBehavior extends CoordinatorLayout.Behavior<MonthPager> {
         parent.onLayoutChild(child, layoutDirection);
         child.offsetTopAndBottom(top);
         return true;
+    }
+
+    private float downX, downY, lastY, lastTop;
+    private boolean isVerticalScroll;
+    private boolean directionUpa;
+
+    @Override
+    public boolean onTouchEvent(CoordinatorLayout parent, MonthPager child, MotionEvent ev) {
+        if (downY > lastTop) {
+            return false;
+        }
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (isVerticalScroll) {
+                    if (ev.getY() > lastY) {
+                        Utils.setScrollToBottom(true);
+                        directionUpa = false;
+                    } else {
+                        Utils.setScrollToBottom(false);
+                        directionUpa = true;
+                    }
+
+                    if (lastTop < child.getViewHeight() / 2 + child.getCellHeight() / 2) {
+                        //这里表示本来是收缩状态
+                        if (ev.getY() - downY <= 0 || Utils.loadTop() >= child.getViewHeight()) {
+                            //向上滑或者已展开了
+                            lastY = ev.getY();
+                            return true;
+                        }
+                        if (ev.getY() - downY + child.getCellHeight() >= child.getViewHeight()) {
+                            //将要滑过头了
+                            saveTop(child.getViewHeight());
+                            Utils.scrollTo(parent, (RecyclerView) parent.getChildAt(1), child.getViewHeight(), 10);
+                            isVerticalScroll = false;
+                        } else {
+                            //正常下滑
+                            saveTop((int) (child.getCellHeight() + ((ev.getY() - downY))));
+                            Utils.scroll(parent.getChildAt(1), (int) (lastY - ev.getY()),
+                                    child.getCellHeight(), child.getViewHeight());
+                        }
+                    } else {
+                        if (ev.getY() - downY >= 0 || Utils.loadTop() <= child.getCellHeight()) {
+                            lastY = ev.getY();
+                            return true;
+                        }
+
+                        if (ev.getY() - downY + child.getViewHeight() <= child.getCellHeight()) {
+                            //将要滑过头了
+                            saveTop(child.getCellHeight());
+                            Utils.scrollTo(parent, (RecyclerView) parent.getChildAt(1), child.getCellHeight(), 10);
+                            isVerticalScroll = false;
+                        } else {
+                            //正常上滑
+                            saveTop((int) (child.getViewHeight() + ((ev.getY() - downY))));
+                            Utils.scroll(parent.getChildAt(1), (int) (lastY - ev.getY()),
+                                    child.getCellHeight(), child.getViewHeight());
+                        }
+                    }
+
+                    lastY = ev.getY();
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isVerticalScroll) {
+
+                    child.setScrollable(true);
+
+                    CalendarViewAdapter calendarViewAdapter =
+                            (CalendarViewAdapter) child.getAdapter();
+                    if (calendarViewAdapter != null) {
+                        if (directionUpa) {
+                            Utils.setScrollToBottom(true);
+                            calendarViewAdapter.switchToWeek(child.getRowIndex());
+                            Utils.scrollTo(parent, (RecyclerView) parent.getChildAt(1), child.getCellHeight(), 300);
+                        } else {
+                            Utils.setScrollToBottom(false);
+                            calendarViewAdapter.switchToMonth();
+                            Utils.scrollTo(parent, (RecyclerView) parent.getChildAt(1), child.getViewHeight(), 300);
+                        }
+                    }
+
+                    isVerticalScroll = false;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    private void saveTop(int top) {
+        Utils.saveTop(top);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(CoordinatorLayout parent, MonthPager child, MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = ev.getX();
+                downY = ev.getY();
+                lastTop = Utils.loadTop();
+                lastY = downY;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (downY > lastTop) {
+                    return false;
+                }
+                if (Math.abs(ev.getY() - downY) > 25 && Math.abs(ev.getX() - downX) <= 25
+                        && !isVerticalScroll) {
+                    isVerticalScroll = true;
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isVerticalScroll) {
+                    isVerticalScroll = false;
+                    return true;
+                }
+                break;
+        }
+        return isVerticalScroll;
     }
 
     private int dependentViewTop = -1;
